c06361181949a6b4c714212153482cb4bb61e02c
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-09-12 00:53:24 +0300

    Task 4

---
+++ Tyuiu.RadochinaAP.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.RadochinaAP.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.RadochinaAP.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+
+            Console.WriteLine(DataService.Subtraction(15, 5));
+
+            Console.WriteLine(DataService.Multiplication(10, 10));
+
+            Console.WriteLine(DataService.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
