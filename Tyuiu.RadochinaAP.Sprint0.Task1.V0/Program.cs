b83358b487245fd10334c403cdb901a69e978a98
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-09-11 22:30:10 +0300

    Task1

---
+++ Tyuiu.RadochinaAP.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.RadochinaAP.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Арина";
+            int age = 21;
+            Console.WriteLine("Добрый день, я " + name + ", мне " + age + " год ^^");
+            Console.ReadKey();
+        }
+    }
+}
