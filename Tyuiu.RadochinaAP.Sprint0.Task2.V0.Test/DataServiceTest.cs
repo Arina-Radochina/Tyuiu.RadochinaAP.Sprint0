b92416b3c03b397ef2655bb5745a77ed2ea66939
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-09-11 23:24:52 +0300

    Task2

---
+++ Tyuiu.RadochinaAP.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.RadochinaAP.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.RadochinaAP.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Арина";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Арина", res);
+        }
+    }
+}
